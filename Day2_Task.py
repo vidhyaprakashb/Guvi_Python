768056d624e2dcb37b6fa2c26b21168833749fd9
vidhyaprakash.b@gmail.com <vidhyaprakash.b@gmail.com>
2025-01-04 17:44:36 +0530

    Task2 added

---
+++ Day2_Task.py
@@ -0,0 +1,30 @@
+# n =10
+# print("N value is ",n)
+# sum =0
+# for i in range(1, n+1):
+#     sum += i
+# print("Sum of all numbers from 1 to ",n, " is ", sum)
+
+# n=5
+# number = n
+# factorial = 1
+# while n>1:
+#     print(n, "*", factorial)
+#     factorial *= n
+#     n -= 1
+#     print("= ",factorial)
+# print("Factorial of a given number ",number, " is :",factorial)
+
+# n = 7
+# fibonacci_seq = []
+# a,b = 0,1
+# for i in range(n):
+#     fibonacci_seq.append(a)
+#     a,b = b,a+b
+# print("Fibonacci Series for given number ",n, " is ", fibonacci_seq)
+
+
+# number = 3
+#
+# for i in range(1,11):
+#     print(f'{number} x {i} = {number*i}')
